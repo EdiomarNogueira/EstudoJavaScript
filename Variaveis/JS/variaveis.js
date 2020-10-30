f0dad7ea27b66831da6f30a2411217c6fb24c18e
Ediomar.Nogueira <ediomar_ti@hotmail.com>
2020-10-30 00:44:25 -0300

    Atualização

---
+++ Variaveis/JS/variaveis.js
@@ -0,0 +1,13 @@
+//string
+var texto = "Curso de JavaScript"
+
+//number
+var numeroInteiro = -7
+var numeroFracionado = 10.75
+
+//boolean
+var teste = false //true -> verdadeiro / false ->  falso
+
+//alert(numeroFracionado) //abrir um dialog
+//document.write(texto)
+console.log(numeroInteiro)
