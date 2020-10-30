ddb81e3dbf17030695d6fa85cb5df1c458eac66c
Ediomar.Nogueira <ediomar_ti@hotmail.com>
2020-10-30 00:02:05 -0300

    Commit inicial

---
+++ PrimeiroCodigo/JS/meu_script.js
@@ -0,0 +1,15 @@
+alert('Olá, seja bem vindo ao curso!')
+
+
+/* 
+    Selecionar um elemento no DOM
+    atualizar o valor desse elemento com uma string
+*/
+
+document.getElementById('nome').value = 'Oi'
+//Comentando um linha
+
+/*
+Comentando um bloco
+de texto
+*/ 
