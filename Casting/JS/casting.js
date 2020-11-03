49f47e45409893829e2925183285034a76265eb6
Ediomar.Nogueira <ediomar_ti@hotmail.com>
2020-11-03 03:28:06 -0300

    atualização

---
+++ Casting/JS/casting.js
@@ -0,0 +1,16 @@
+//var variavel1 = prompt("digite algum numero:")
+//var variavel2 = prompt("digite outro numero")
+
+//variavel1 = parseInt(variavel1)
+//variavel2 = parseInt(variavel2)
+
+//passeFloat
+
+var variavel1 = 10
+var variavel2 = 20
+
+console.log(variavel1)
+console.log(variavel2)
+
+//document.write(variavel1 + variavel2)
+document.write(variavel1.toString()+ variavel2)
